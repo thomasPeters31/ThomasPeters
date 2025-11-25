1f2d31a59a11cb9b3d051e829c57b09c2578a44c
Thomas <t.peters.31@outlook.com>
2025-11-25 20:23:20 +0000

    hides header when on mobile

--- script.js
+++ script.js
@@ -14,14 +14,3 @@ const appearOnScroll = new IntersectionObserver(function(entries, observer){
 }, appearOptions);
 
 faders.forEach(fader => appearOnScroll.observe(fader));
-
-// Mobile nav toggle for the collapsible header menu
-const menuBtn = document.querySelector('.menu-toggle');
-const nav = document.getElementById('site-nav');
-
-if (menuBtn && nav) {
-  menuBtn.addEventListener('click', () => {
-    const isOpen = document.body.classList.toggle('nav-open');
-    menuBtn.setAttribute('aria-expanded', String(isOpen));
-  });
-}
